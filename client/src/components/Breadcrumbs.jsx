4c6f5337c02eb804f51b9206d2539f59491536de
G1q <beresteanu.geroge@gmail.com>
2024-03-11 13:19:53 +0200

    Create breadcrumbs component

---
+++ client/src/components/Breadcrumbs.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom'
+import { LuChevronsRight } from 'react-icons/lu'
+import './styles/Breadcrumbs.css'
+
+const Breadcrumbs = ({ type, category, parent }) => {
+	let mainCategory = ''
+
+	if (type === 'muscles')
+		mainCategory = {
+			url: '/muscles',
+			title: 'Muscle HUB',
+		}
+
+	return (
+		<ul className="page__breadcrumbs">
+			<Link to={mainCategory.url}>{mainCategory.title}</Link>
+			<LuChevronsRight />
+			{category.parent !== 0 && (
+				<>
+					<li>
+						<Link to={`/muscles/${parent.slug}`}>{parent.title}</Link>
+					</li>
+					<LuChevronsRight />
+				</>
+			)}
+			<li>
+				<p>{category.title}</p>
+			</li>
+		</ul>
+	)
+}
+
+export default Breadcrumbs
